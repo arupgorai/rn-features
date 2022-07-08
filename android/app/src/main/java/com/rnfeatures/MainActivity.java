79de063d21a70bdcffa0dd1d4a5f24f1a350e8db
Arup Gorai 😎 <arup.gorai@digital.datamatics.com>
2022-07-09 00:03:01 +0530

    navigation pkgs added

--- android/app/src/main/java/com/rnfeatures/MainActivity.java
+++ android/app/src/main/java/com/rnfeatures/MainActivity.java
@@ -3,6 +3,7 @@ package com.rnfeatures;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -25,6 +26,11 @@ public class MainActivity extends ReactActivity {
     return new MainActivityDelegate(this, getMainComponentName());
   }
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
   public static class MainActivityDelegate extends ReactActivityDelegate {
     public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
       super(activity, mainComponentName);
@@ -44,5 +50,8 @@ public class MainActivity extends ReactActivity {
       // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
       return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     }
+
+    
+
   }
 }
