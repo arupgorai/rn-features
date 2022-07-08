df5ac6e02115aa4e37d89b33478a1f0f00d2383a
Arup Gorai 😎 <arup.gorai@digital.datamatics.com>
2022-07-08 10:51:49 +0530

    custom fonts added

--- App.js
+++ App.js
@@ -1,24 +1,50 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
-// import Icon from 'react-native-vector-icons/Ionicons';
-// import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
+import {SafeAreaView, View, Text, TouchableOpacity} from 'react-native';
 import Icon from './constant/icons';
 
 export default function () {
   return (
-    <SafeAreaView>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+      }}>
       <View>
-        <Text>React Native</Text>
-        {/* <Icon name="md-menu" size={30} /> */}
-        {/* <Icon2 name="menu" size={30} /> */}
-        <Icon
-          type="Entypo"
-          name="chevron-small-down"
-          size={25}
-          color="red"
-          style={{marginTop: 10}}
-        />
+        <Text
+          style={{
+            fontSize: 30,
+            color: '#20315f',
+            fontFamily: 'Roboto-BoldItalic',
+          }}>
+          React Native
+        </Text>
       </View>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#AD40AF',
+          padding: 20,
+          width: '90%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          borderRadius: 5,
+        }}>
+        <Text
+          style={{
+            fontSize: 18,
+            color: '#fff',
+            fontFamily: 'Roboto-Italic',
+          }}>
+          Let's Begin
+        </Text>
+        <Icon
+          type="MaterialIcons"
+          name="arrow-forward-ios"
+          size={22}
+          color="white"
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
