b612c933713223948b7cfb66234f5f89fb49fe8b
Arup Gorai 😎 <arup.gorai@digital.datamatics.com>
2022-07-09 10:30:09 +0530

    infinit scrolling done

--- App.js
+++ App.js
@@ -1,65 +1,13 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, View, Text} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
+import LoadMoreOnScroll from './src/components/LoadMoreOnScroll';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View style={styles.headerContainer}>
-        <Text style={styles.heading}>Welcome to React Native</Text>
-        <Text style={styles.subHeading}>Features List</Text>
-      </View>
-      <ScrollView style={styles.listContainer}>
-        <View style={styles.listContainerWrap}>
-          <View style={styles.itemWrap}>
-            <Text style={styles.featureItem}>1. React Navit Vector Icons</Text>
-            <Text style={styles.branchName}>RNVectorIcons</Text>
-          </View>
-        </View>
-      </ScrollView>
+      <LoadMoreOnScroll />
     </SafeAreaView>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  headerContainer: {
-    backgroundColor: '#e2e2e2',
-    paddingVertical: 20,
-  },
-  heading: {
-    fontSize: 52,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#222',
-  },
-  subHeading: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: 'tomato',
-    marginTop: 15,
-    textDecorationLine: 'underline',
-  },
-  listContainer: {
-    height: '100%',
-  },
-  listContainerWrap: {
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-  },
-  itemWrap: {
-    marginBottom: 6,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  featureItem: {
-    fontSize: 14,
-  },
-  branchName: {
-    fontSize: 16,
-    fontStyle: 'italic',
-    fontWeight: 'bold',
-  },
-});
---
+++ src/components/LoadMoreOnScroll.js
@@ -0,0 +1,102 @@
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+const LoadMoreOnScroll = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [pageNo, setPageNo] = useState(1);
+
+  const getData = async () => {
+    console.log('getData CALLED');
+    const apiURL = `${BASE_URL}/photos?_limit=10&_page=${pageNo}`;
+
+    try {
+      const resJSON = await fetch(apiURL);
+      const response = await resJSON.json();
+      setData(data.concat(response));
+    } catch (error) {
+      console.log('Error :-', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const renderItem = ({item}) => {
+    return (
+      <View style={styles.itemRow}>
+        <Image source={{uri: item.url}} style={styles.itemImage} />
+        <Text style={styles.itemText}>
+          {item.id}. {item.title}
+        </Text>
+      </View>
+    );
+  };
+
+  const renderFooter = () => {
+    return isLoading ? (
+      <View style={styles.loader}>
+        <ActivityIndicator size="large" />
+      </View>
+    ) : null;
+  };
+
+  const handleMore = () => {
+    setPageNo(prevState => prevState + 1);
+    setIsLoading(true);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getData();
+  }, [pageNo]);
+
+  return (
+    <FlatList
+      style={styles.container}
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={(item, index) => index.toString()}
+      ListFooterComponent={renderFooter}
+      onEndReached={handleMore}
+      onEndReachedThreshold={0.5}
+    />
+  );
+};
+
+export default LoadMoreOnScroll;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    backgroundColor: '#f5fcff',
+  },
+  itemRow: {
+    borderBottomColor: '#ccc',
+    marginBottom: 10,
+    borderBottomWidth: 1,
+  },
+  itemImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+  },
+  itemText: {
+    fontSize: 16,
+    padding: 5,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  loader: {
+    marginTop: 10,
+    alignItems: 'center',
+  },
+});
