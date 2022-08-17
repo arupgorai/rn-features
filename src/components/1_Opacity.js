9dc17462fc5cca28373af9d68789d7db06c27353
Arup Gorai 😎 <arup.gorai@digital.datamatics.com>
2022-08-17 15:56:18 +0530

    update

--- src/components/1_Opacity.js
+++ src/components/1_Opacity.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -10,19 +10,18 @@ import Animated, {
 const Opacity = () => {
   const animation = useSharedValue(1);
   const rStyle = useAnimatedStyle(() => ({
-    opacity: withTiming(animation.value, {duration: 3000}, () => {
+    opacity: withTiming(animation.value, {duration: 2000}, () => {
       animation.value = 1;
     }),
   }));
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Animated.View
-        style={[{width: 150, height: 150, backgroundColor: 'blue'}, rStyle]}
-      />
-      <TouchableOpacity onPress={() => (animation.value = 0)}>
-        <Text>animate</Text>
-      </TouchableOpacity>
+      <TouchableWithoutFeedback onPress={() => (animation.value = 0)}>
+        <Animated.View
+          style={[{width: 150, height: 150, backgroundColor: 'blue'}, rStyle]}
+        />
+      </TouchableWithoutFeedback>
     </View>
   );
 };
